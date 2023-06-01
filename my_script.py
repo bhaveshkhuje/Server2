199fbfbbd9db4ab53d04bec7c9f5b9bbc51165cf
bhaveshkhuje <89595788+bhaveshkhuje@users.noreply.github.com>
2023-06-01 19:29:39 +0530

    Add files via upload

---
+++ my_script.py
@@ -0,0 +1,14 @@
+def process_input(user_input):
+    # Replace this with your own code that processes the user's input
+    
+    if user_input == "SPF 50 Sunscreen":
+        import webbrowser
+
+        filename = 'FF.html'
+        url = 'C:\Users\BHAVESH\Desktop\demo\FF.html' + filename
+
+        webbrowser.open(url)
+    
+
+    
+    

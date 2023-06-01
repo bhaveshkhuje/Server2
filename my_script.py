59676dc5d4bced850f0a66df806b2f34db03e3d7
bhaveshkhuje <89595788+bhaveshkhuje@users.noreply.github.com>
2023-06-01 19:35:24 +0530

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -1,12 +1,10 @@
 def process_input(user_input):
     # Replace this with your own code that processes the user's input
-    
+     
     if user_input == "SPF 50 Sunscreen":
         import webbrowser
 
-        filename = 'FF.html'
-        url = 'C:\Users\BHAVESH\Desktop\demo\FF.html' + filename
-
+        url = 'https://www.google.com'
         webbrowser.open(url)
     
 
